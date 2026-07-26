1e5eb50f595c7401211f60bd1e7f69422792a31f
Aisyah Alya Khumaira <aisyahkhumaira428@gmail.com>
2026-07-26 23:00:43 +0700

    Add carousel

---
+++ js/layout.js
@@ -0,0 +1,20 @@
+const track = document.querySelector(".slider-track");
+const next = document.querySelector(".layout-next");
+const prev = document.querySelector(".layout-prev");
+
+let position = 0;
+const maxSlide = 300;
+
+next.addEventListener("click", () => {
+    if (position > -maxSlide) {
+        position -= maxSlide;
+        track.style.transform = `translateX(${position}px)`;
+    }
+});
+
+prev.addEventListener("click", () => {
+    if (position < 0) {
+        position += maxSlide;
+        track.style.transform = `translateX(${position}px)`;
+    }
+});
