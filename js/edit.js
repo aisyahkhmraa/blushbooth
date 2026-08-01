694304a672d3bd296c315bd4081056f429acb922
Aisyah Alya Khumaira <aisyahkhumaira428@gmail.com>
2026-08-01 21:22:14 +0700

    update edit page

--- js/edit.js
+++ js/edit.js
@@ -1,3 +1,14 @@
 const photos = JSON.parse(localStorage.getItem("photos"));
+const template = document.querySelector("#template");
 
 console.log(photos);
+
+for (let i = 1; i <= 6; i++) {
+  const img = document.createElement("img");
+
+  img.src = `assets/templates/template${i}.png`;
+
+  img.classList.add("template-option");
+
+  template.appendChild(img);
+}
